8eabe91a7c85ef7a42b7f0a6701ff0fa31092d82
juhfrancic <juliafrancictostes@gmail.com>
2025-11-22 16:06:10 -0300

    Dando merge na brach Veiculos na Master

---
+++ Locadora.Models/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Funcionario
+    {
+        public int FuncionarioID { get; private set; }
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Email { get; private set; }
+        public decimal? Salario { get; private set; }
+
+        public Funcionario(string nome, string cPF, string email)
+        {
+            Nome = nome;
+            CPF = cPF;
+            Email = email;
+        }
+
+        public Funcionario(string nome, string cPF, string email, decimal? salario) : this(nome, cPF, email)
+        {
+            Salario = salario;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, CPF: {CPF}, Email: {Email}, Salario: {Salario}";
+        }
+    }
+
+}
